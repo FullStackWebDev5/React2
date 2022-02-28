8db93762059da8649259f855cff8fca2288d5500
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-02-28 23:12:25 +0530

    React: Rendering Loops, key, React Lifecycle Theory, react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
-import CounterClass from './components/CounterClass'
-import HelloClass from './components/HelloClass'
-import CollegeStudents from './components/CollegeStudents/CollegeStudents'
+// import CounterClass from './components/CounterClass'
+// import HelloClass from './components/HelloClass'
+// import CollegeStudents from './components/CollegeStudents/CollegeStudents'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import BootstrapBasics from './components/BootstrapBasics'
+
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <CollegeStudents />
+        <BootstrapBasics />
+        {/* <CollegeStudents /> */}
         {/* <CounterClass /> */}
         {/* <HelloClass /> */}
         {/* <HelloClass greetTo='Africa' />
--- src/components/CollegeStudents/CollegeStudents.js
+++ src/components/CollegeStudents/CollegeStudents.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './CollegeStudents.css'
+import devloperImage from '../../images/developer.jpeg'
 
 class CollegeStudents extends React.Component {
   constructor(props) {
@@ -7,6 +8,7 @@ class CollegeStudents extends React.Component {
     this.state = {
       students: [
         {
+          id: 1,
           name: 'Lakshman',
           bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, tenetur',
           course: 'Full Stack Web Dev',
@@ -14,36 +16,41 @@ class CollegeStudents extends React.Component {
           batch: 'Batch 11'
         },
         {
+          id: 2,
+          name: 'Ram',
+          bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, tenetur consectetur adipisicing elit. Aliquam, tenetur',
+          course: 'Front End Web Dev',
+          techStack: 'MEVN',
+          batch: 'Batch 12'
+        },
+        {
+          id: 3,
           name: 'Lakshman',
           bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, tenetur',
           course: 'Full Stack Web Dev',
           techStack: 'MERN',
           batch: 'Batch 11'
-        }
+        },
       ]
     }
   }
   
   render(){
     const { students } = this.state
-    const student1 = students[0]
-    const student2 = students[1]
+
     return (
       <>
-        <div className="student-data">
-          <h2>{student1.name}</h2>
-          <p>{student1.bio}</p>
-          <h3>Course: {student1.course}</h3>
-          <h3>Tech Stack: {student1.techStack}</h3>
-          <h3>Batch: {student1.batch}</h3>
-        </div>
-        <div className="student-data">
-          <h2>{student2.name}</h2>
-          <p>{student2.bio}</p>
-          <h3>Course: {student2.course}</h3>
-          <h3>Tech Stack: {student2.techStack}</h3>
-          <h3>Batch: {student2.batch}</h3>
-        </div>
+      {students.map((student, index) => {
+        return (
+          <div className="student-data" key={index}>
+            <h2>{student.name}</h2>
+            <p>{student.bio}</p>
+            <h3>Course: {student.course}</h3>
+            <h3>Tech Stack: {student.techStack}</h3>
+            <h3>Batch: {student.batch}</h3>
+          </div>
+        )})}
+        <img src={devloperImage} alt='Developer' />
       </>
     );
   }
